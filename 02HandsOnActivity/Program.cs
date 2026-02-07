d245925c29bf647bfe03f72f11af006d15fe50cc
emp <emp@nwjnsbrappy.local>
2026-02-07 18:51:15 +0800

    Initial Commit: Added Two Projects

---
+++ 02HandsOnActivity/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace DataTypesApp
+{
+    class DataTypesProgram
+    {
+        static void Main(string[] args)
+        {
+            int numberApple;
+            double priceApple;
+            int convertedPriceApple;
+
+            Console.WriteLine("WELCOME TO APOL STORE!");
+
+            Console.WriteLine("Enter the pieces of apple.");
+            Console.Write(": ");
+            numberApple = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the total amount of " + numberApple + " apple(s).");
+            Console.Write(": ");
+            priceApple = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("The total price of " + numberApple + " apple(s) is " + priceApple + " PHP");
+
+            Console.WriteLine("\nITEM DETAILS");
+            convertedPriceApple = (int)priceApple;
+            
+            Console.WriteLine("The value of original price is " + priceApple);
+            Console.WriteLine("The value of converted price is " + convertedPriceApple);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 03PerformanceTask/PrelimProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            int grade1, grade2, grade3, grade4, grade5;
+            double initialGrade;
+            double averageGrade;
+            double convertedAverageGrade;
+
+            Console.WriteLine("WELCOME TO GRADE CALCULATOR");
+
+            Console.WriteLine("Enter 5 grades for each new line.");
+            Console.Write(": ");
+            grade1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write(": ");
+            grade2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write(": ");
+            grade3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write(": ");
+            grade4 = Convert.ToInt32(Console.ReadLine());
+            Console.Write(": ");
+            grade5 = Convert.ToInt32(Console.ReadLine());
+
+            initialGrade = grade1 + grade2 + grade3 + grade4 + grade5;
+            averageGrade = initialGrade / 5;
+            convertedAverageGrade = Math.Round(averageGrade);
+            
+            Console.WriteLine("The average grade is " + averageGrade + " rounded off to " + convertedAverageGrade);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
