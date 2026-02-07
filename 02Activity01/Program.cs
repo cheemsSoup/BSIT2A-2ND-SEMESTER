73c64d4a4422c9d3548eacabfc250d0ab0eb96ac
emp <emp@nwjnsbrappy.local>
2026-02-07 19:21:22 +0800

    Added Two Activities

---
+++ 02Activity01/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace DataTypeApp
+{
+    class DataTypeProgram
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            String address;
+            string item;
+            double price;
+            bool payment = true;
+            int estDelivery = 3;
+            DateTime today = DateTime.Now;
+
+            Console.WriteLine("WELCOME TO APOL STORE!");
+
+            Console.WriteLine("I am your assistant, please enter your name.");
+            Console.Write(": ");
+            name = Console.ReadLine()!;
+            Console.WriteLine("Hello, " + name + ".");
+
+            Console.WriteLine("What would you like to get in our catalog? ");
+            Console.Write(": ");
+            item = Console.ReadLine()!;
+
+            Console.WriteLine("How much is the price? ");
+            Console.Write(": ");
+            price = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Where can we ship it? ");
+            Console.Write(": ");
+            address = Console.ReadLine()!;
+
+            Console.WriteLine("\nITEM DETAILS");
+
+            Console.WriteLine(item + " - " + price + " PHP");
+
+            Console.WriteLine("Name: " + name);
+
+            Console.WriteLine("Address: " + address);
+
+            Console.WriteLine("Payment Status: " + payment);
+
+            Console.WriteLine("Date of Order: " + today);
+
+            Console.WriteLine("Estimated Time of Delivery: " + estDelivery + " Business Days");
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 03Activity01/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+using System;
+namespace MathApp
+{
+    class MathProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO CALC");
+            Console.WriteLine("Calc is short for calculator btw");
+            Console.WriteLine("Im just using slang");
+
+            Console.WriteLine("\n[1] Power");
+            Console.WriteLine("Enter base number.");
+            Console.Write(": ");
+            double baseNum = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter exponent.");
+            Console.Write(": ");
+            double expNum = Convert.ToDouble(Console.ReadLine());
+            double powerResult = Math.Pow(baseNum, expNum);
+            Console.WriteLine(baseNum + " raised to " + expNum + " is " + powerResult);
+
+            Console.WriteLine("\n[2] Square Root");
+            Console.WriteLine("Enter base number.");
+            Console.Write(": ");
+            double sqrNum = Convert.ToDouble(Console.ReadLine());
+            double sqrResult = Math.Sqrt(sqrNum);
+            Console.WriteLine("The square root of " + sqrNum + " is " + sqrResult);
+
+            Console.WriteLine("\n[3] Absolute Value");
+            Console.WriteLine("Enter negative number.");
+            Console.Write(": ");
+            double negativeNum = Convert.ToDouble(Console.ReadLine());
+            double absResult = Math.Abs(negativeNum);
+            Console.WriteLine("The absolute number of " + negativeNum + " is " + absResult);
+
+            Console.WriteLine("\n[4] Round UP");
+            Console.WriteLine("Enter a decimal to round UP (ceiling).");
+            Console.Write(": ");
+            double ceilingNum = Convert.ToDouble(Console.ReadLine());
+            double ceilingResult = Math.Ceiling(ceilingNumNum);
+            Console.WriteLine("The ceiling of " + ceilingNumNum + " is " + ceilingNum);
+
+            Console.WriteLine("\n[5] Round OFF");
+            Console.WriteLine("Enter a decimal number to round off.");
+            Console.Write(": ");
+            double roundNum = Convert.ToDouble(Console.ReadLine());
+            double roundResult = Math.Abs(roundNum);
+            Console.WriteLine("The rounded value of " + roundNum + " is " + roundResult);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
